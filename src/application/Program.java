675facbde6ecd4011e7a32205d80f77bdf651d65
Victor Holanda <jvictorweb21@gmail.com>
2022-03-13 15:29:34 -0300

    Modified program: added income for month and year and fixed parse date

--- src/application/Program.java
+++ src/application/Program.java
@@ -16,16 +16,17 @@ public class Program {
 	public static void main(String[] args) throws ParseException {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/mm/yyyy)");
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
 		String departamentName;
 		String workerName;
 		String workerLevel;
+		String monthAndYear;
 		double baseSalary;
 				
 		System.out.print("Enter departament's name: ");
 		departamentName = sc.nextLine();
-		System.out.print("Enter worker data: ");
+		System.out.println("Enter worker data: ");
 		System.out.print("Name: ");
 		workerName = sc.nextLine();		
 		System.out.print("Level: ");
@@ -55,6 +56,22 @@ public class Program {
 			HourContract contract = new HourContract(contractDate,valuePerHour,hours);
 			worker.addContract(contract);
 		}
+		
+		System.out.println();
+		System.out.print("Enter month and year to calculate income(MM/YYY): ");
+		monthAndYear = sc.next();
+		
+		int month = Integer.parseInt(monthAndYear.substring(0,2));
+		int year =  Integer.parseInt(monthAndYear.substring(3));
+		
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Departament: " + worker.getDepartament().getName());
+		System.out.println(
+							"Income for " +
+							monthAndYear + 
+							": " + 
+							String.format("%.2f", worker.income(year, month))
+						);
 		
 		sc.close();
 		
