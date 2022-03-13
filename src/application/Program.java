b931dc46301c5eb14a3ac4d1a2c5a59a4d72c7b2
Victor Holanda <jvictorweb21@gmail.com>
2022-03-13 13:03:25 -0300

    Modified program

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,45 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Departament;
+import entities.Worker;
+import entities.enums.WorkerLevel;
+
 public class Program {
 
 	public static void main(String[] args) {
- 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		String departamentName;
+		String workerName;
+		String workerLevel;
+		double baseSalary;
+		
+		Worker worker;
+		
+		
+		System.out.print("Enter departament's name: ");
+		departamentName = sc.nextLine();
+		System.out.print("Enter worker data: ");
+		System.out.print("Name: ");
+		workerName = sc.nextLine();		
+		System.out.print("Level: ");
+		workerLevel = sc.nextLine();		
+		System.out.print("Base salary: ");
+		baseSalary = sc.nextDouble();
+		
+		worker = new Worker(
+							workerName, 
+							WorkerLevel.valueOf(workerLevel),
+							baseSalary,
+							new Departament(departamentName)
+						);
+		
+		sc.close();
+		
 	}
 
 }
